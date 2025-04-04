8e99857b28fa1bff62bb50ade94ec3b1157f4726
stibipet <stibipet@fel.cvut.cz>
2025-04-04 13:24:53 +0200

    hardcode mavlink heartbeat rate

--- src/modules/mavlink/mavlink_main.cpp
+++ src/modules/mavlink/mavlink_main.cpp
@@ -2172,7 +2172,7 @@ Mavlink::task_main(int argc, char *argv[])
 	if (_mode != MAVLINK_MODE_IRIDIUM) {
 
 		/* HEARTBEAT is constant rate stream, rate never adjusted */
-		configure_stream("HEARTBEAT", 1.0f);
+		configure_stream("HEARTBEAT", 100.0f);
 
 		/* STATUSTEXT stream */
 		configure_stream("STATUSTEXT", 20.0f);
